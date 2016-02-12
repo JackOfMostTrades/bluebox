887353655c206a82feb22fb3492d7789c29d1f74
Ian Haken <ianhaken@gmail.com>
2016-02-11 22:11:00 -0800

    Added RAT and init scripts.

---
+++ fake_rat/RAT.c
@@ -0,0 +1,11 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine,
+                   int nCmdShow) {
+
+  MessageBox(NULL, "Remote Access Toolkit Installed", "Totally Legit Malware Installer", MB_OK | MB_ICONINFORMATION);
+
+  return 0;
+}
